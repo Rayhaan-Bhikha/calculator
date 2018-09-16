061960c8c772327b7476543cddb10c28710babff
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-09-16 13:04:09 +0100

    user input now has verbose option

--- src/main/rayhaanbhikha/calculator/Calculator.java
+++ src/main/rayhaanbhikha/calculator/Calculator.java
@@ -2,15 +2,27 @@ package rayhaanbhikha.calculator;
 
 public class Calculator {
 
-    public Calculator() {}
+    private Boolean verbose = false;
+    private UserInput userInput;
+    private ShuntingYard sy;
+    private ReducePostFix rp;
+
+    public Calculator(){}
+
+    public Calculator(Boolean verbose) {
+        this.verbose = verbose;
+    }
 
     public String evaluateExpression(String input) {
+        userInput = new UserInput(input);
+        sy = new ShuntingYard(userInput.getFilteredInput());
 
-        UserInput userInput = new UserInput(input);
+        if(verbose) {
+            userInput.printParsedUserInput();
+        }
 
-        ShuntingYard sy = new ShuntingYard(userInput.getFilteredInput());
 
-        ReducePostFix rp = new ReducePostFix(sy.getPostFixInput());
+        rp = new ReducePostFix(sy.getPostFixInput());
 
         return rp.getFinalAnswer();
     }
--- src/main/rayhaanbhikha/calculator/UserInput.java
+++ src/main/rayhaanbhikha/calculator/UserInput.java
@@ -14,7 +14,7 @@ public class UserInput {
 
     public void filterInput(String input) {
         filteredInput = new ArrayList<>();
-        String accum = "";
+        String accumulated = "";
         for(int i=0; i<input.length(); i++) {
             char currentInput = input.charAt(i);
             switch(currentInput) {
@@ -22,35 +22,33 @@ public class UserInput {
                 case '3': case '4': case '5':
                 case '6': case '7': case '8':
                 case '9':
-                    accum += currentInput;
+                    accumulated += currentInput;
                     break;
                 case '%':
                     /**
                      * by this point the filteredInput array list should have
                      * string char string
                      * */
-//                    computePercentage()
                     if(filteredInput.size() > 1 && filteredInput.get(filteredInput.size() - 1) instanceof Character && (Character) filteredInput.get(filteredInput.size() - 1) == '(') {
-                        System.out.println("Bracket exists: "+  filteredInput.get(filteredInput.size() - 1) + accum + currentInput);
-//                        15 %
-                        accum = Double.toString(Double.parseDouble(accum)/100);
+                        System.out.println("Bracket exists: "+  filteredInput.get(filteredInput.size() - 1) + accumulated + currentInput);
+                        accumulated = Double.toString(Double.parseDouble(accumulated)/100);
                     } else {
-                        accum += currentInput;
+                        accumulated += currentInput;
                     }
                     break;
                 case ' ':
                     break;
                 default:
                     /* if it's an operand or bracket
-                        then add accum to filtered input
-                        and make accum empty.*/
-                    filteredInput.add(accum);
-                    accum = "";
+                        then add accumulated to filtered input
+                        and make accumulated empty.*/
+                    filteredInput.add(accumulated);
+                    accumulated = "";
                     filteredInput.add(currentInput); // add operand on it's own
                     break;
             }
         }
-        filteredInput.add(accum);
+        filteredInput.add(accumulated);
         filteredInput.removeIf(i -> i.equals("")); // remove all empty string in Arraylist
     }
 
@@ -59,6 +57,7 @@ public class UserInput {
     }
 
     public void printParsedUserInput(){
+        System.out.print("Parsed User input: ");
         for(Object i: filteredInput) {
             System.out.print(i.toString() + " ");
         }
