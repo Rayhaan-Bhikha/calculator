b37820bf87fc179e9e7ba3131ab4064f27b92b3c
Rayhaan Bhikha <rayhaanbhikha3@hotmail.com>
2018-09-16 12:51:52 +0100

    Calculator class refactored

--- src/main/rayhaanbhikha/calculator/Calculator.java
+++ src/main/rayhaanbhikha/calculator/Calculator.java
@@ -2,20 +2,16 @@ package rayhaanbhikha.calculator;
 
 public class Calculator {
 
-    private UserInput userInput;
-    private ShuntingYard sy;
-    private ReducePostFix rp;
-    private String answer;
+    public Calculator() {}
 
-    public Calculator(String input) {
-        this.userInput = new UserInput(input);
-        sy = new ShuntingYard(userInput.getFilteredInput());
-        System.out.println("Postfix: " + sy.getPostFixInput());
-        rp = new ReducePostFix(sy.getPostFixInput());
-        answer = rp.getFinalAnswer();
-    }
+    public String evaluateExpression(String input) {
 
-    public String getAnswer() {
-        return answer;
+        UserInput userInput = new UserInput(input);
+
+        ShuntingYard sy = new ShuntingYard(userInput.getFilteredInput());
+
+        ReducePostFix rp = new ReducePostFix(sy.getPostFixInput());
+
+        return rp.getFinalAnswer();
     }
 }
